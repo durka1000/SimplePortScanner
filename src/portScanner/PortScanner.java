e4e04df8e88bacc99487d86aa2127bef66c7d0c7
durka1000 <durka000@gmail.com>
2017-03-31 20:45:27 +0200

    added argument and boolean for file usage

--- src/portScanner/PortScanner.java
+++ src/portScanner/PortScanner.java
@@ -9,20 +9,31 @@ public class PortScanner{
 	static int from;
 	static int to;
 	static PortTesterThread porttester;
+	static boolean writeToFile;
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		if (args.length == 0) {
-			System.out.println("PortScanner by durka1000\nInput: [ip-address] [minimal port] [maximal port]");
+			System.out.println("PortScanner by durka1000\nInput: [ip-address] [minimal port] [maximal port] [-f]");
 			Scanner scanner = new Scanner(System.in);
 			address = scanner.next();
 			from = scanner.nextInt();
 			to = scanner.nextInt();
+			if (scanner.next() == "-f") {
+				writeToFile = true;
+			} else {
+				writeToFile = false;
+			}
 			scanner.close();
 		} else {
 			address = args[0];
 			from = Integer.parseInt(args[1]);
 			to = Integer.parseInt(args[2]);
+			if (args[3] == "-f") {
+				writeToFile = true;
+			} else {
+				writeToFile = false;
+			}
 		}
 		
 		Instant anfang = Instant.now();
